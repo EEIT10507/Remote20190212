07480e0ba17e4182759a79cdbe12a3761f192162
吳友廷 <0900075@gmail.com>
2019-02-12 14:33:22 +0800

    change controller.LoginServlet.java delete hello world 第4次

--- LabWebGit/src/main/java/controller/LoginServlet.java
+++ LabWebGit/src/main/java/controller/LoginServlet.java
@@ -22,7 +22,6 @@ public class LoginServlet extends HttpServlet {
 	@Override
 	public void init() throws ServletException {
 		customerService = new CustomerService();
-		System.out.println("Hello world");
 	}
 	
 	@Override
